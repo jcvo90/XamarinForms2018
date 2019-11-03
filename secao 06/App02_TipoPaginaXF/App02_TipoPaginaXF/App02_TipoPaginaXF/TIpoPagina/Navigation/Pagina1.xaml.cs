cad72f1f823b99252bbe1309de3d133de39a6dbb
Android <Android@DESKTOP-684H0BU>
2019-11-03 20:59:03 -0300

    App 02

---
+++ secao 06/App02_TipoPaginaXF/App02_TipoPaginaXF/App02_TipoPaginaXF/TIpoPagina/Navigation/Pagina1.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App02_TipoPaginaXF.TIpoPagina.Navigation
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Pagina1 : ContentPage
+    {
+        public Pagina1()
+        {
+            InitializeComponent();
+        }
+
+        private void MudarParaPagina2(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new pagina2()); 
+        }
+    }
+}
